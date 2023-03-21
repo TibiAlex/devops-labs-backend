0891142e1a5d5d34b7bb777d7380dcbffa3ad5e6
TibiAlex <tibi.alex@yahoo.com>
2023-03-21 13:34:57 +0000

    connect ssh19

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+TAG=$(echo $COMMIT_ID | cut -c1-7)
 if docker ps -f "name=dotnet_backend_application" | grep -q dotnet_backend_application; then
 	docker stop dotnet_backend_application
         docker rm dotnet_backend_application
 fi
-docker run -d --name dotnet_backend_application -p 4000:4000 tibialex2000/dotnet_backend_application:latest
+docker run -d --name dotnet_backend_application -p 4000:4000 tibialex2000/dotnet_backend_application:$TAG
