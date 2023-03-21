2ee646afef735322c9e959687c097efbbc1b2df7
TibiAlex <tibi.alex@yahoo.com>
2023-03-21 12:56:07 +0000

    connect ssh12

--- script.sh
+++ script.sh
@@ -4,4 +4,5 @@ if docker ps -f "name=dotnet_backend_application" | grep -q dotnet_backend_appli
 	docker stop dotnet_backend_application
         docker rm dotnet_backend_application
 fi
+echo $1
 docker run -d --name dotnet_backend_application -p 4000:4000 tibialex2000/dotnet_backend_application:$1
