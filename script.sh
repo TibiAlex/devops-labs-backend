2c2b4b467e88a410878215095e73c62d916868ec
TibiAlex <tibi.alex@yahoo.com>
2023-03-21 22:12:45 +0000

    update:

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ if [ "$container_name" = "dotnet_backend_application" ]; then
 	docker stop dotnet_backend_application
         docker rm dotnet_backend_application
 fi
-docker run -d --name dotnet_backend_application -p 4000:4000 tibialex2000/dotnet_backend_application:$TAG
+docker run -d --name dotnet_backend_application --network devopslabs -p 4000:4000 tibialex2000/dotnet_backend_application:$TAG
